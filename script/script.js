3b8ecc53001f7db0eb25860d93912f4448c16cb8
Den <den204d@gmail.com>
2023-10-11 00:05:09 +0300

    finished index.html and style.css, upd script.js

--- script/script.js
+++ script/script.js
@@ -2,15 +2,17 @@
 
 let tabActive = document.querySelectorAll(".ourServices__items");
 let tabContent = document.querySelectorAll(".tabs__content li");
+let arrow = document.querySelectorAll(".arrow__active");
 
 tabActive.forEach((item) => {
   item.addEventListener('click', () => {
 
     tabActive.forEach((navLink) => {
       navLink.classList.remove('active');
+      navLink.querySelector('.arrow__active').style.visibility = "hidden";
     });
     item.classList.add('active');
-
+    item.querySelector('.arrow__active').style.visibility = "visible";
     const tabName = item.getAttribute('data-tab');
     tabContent.forEach((content) => {
       content.style.display = 'none';
@@ -22,5 +24,32 @@ tabActive.forEach((item) => {
     justify-content: space-between;
     gap: 15px;
     `
+  });
+});
+
+
+let workActive = document.querySelectorAll(".work__items");
+let workContent = document.querySelectorAll(".work__img");
+
+workActive.forEach((item) => {
+  item.addEventListener('click', () => {
+
+    workActive.forEach((navLink) => {
+      navLink.classList.remove('work__active');
+    });
+    item.classList.add('work__active');
+
+    const tabName = item.getAttribute('data-tab');
+    workContent.forEach((content) => {
+      content.style.display = 'none';
+    });
+
+    const tabContentActive = document.querySelectorAll(`[data-tab-content="${tabName}"], [data-parent="${tabName}"]`);
+
+    tabContentActive.forEach((item) => {
+      item.style.cssText = `
+      display: block;
+      `
+    })
   });
 });
